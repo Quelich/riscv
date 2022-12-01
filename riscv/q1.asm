7ad54a3d45689d01b3992cca35e0a001716aa4fc
Emre Kılıç <emre1kilic@outlook.com>
2022-12-01 23:16:33 +0300

    remove unnecessary read strings

--- riscv/q1.asm
+++ riscv/q1.asm
@@ -1,40 +1,44 @@
-
-
-
-.global _start
+ .global _start
 _start:
-        la a0, helloworld	        # prepare to print string  
+        la a0, askint	        # prepare to print string  
         li a7, 4			# print string
         ecall
- 
-        li a0, 0           # File descriptor, 0 for STDIN
-        la a1, str1           # Address of buffer to store string
-        li a2, 255        # Maximum number of chars to store
-        li a7, 63         # System call code for read string
-        ecall
-
-        la a0, str1	        # prepare to print string 
-        li a7, 4			# print string
-        ecall
-
+      
         li a0, 0            # File descriptor, 0 for STDIN
         li a7, 5            # System call code for read integer. The value will be in a0
         ecall
-     
-        li a0, 42
+    
         li a7, 1			# Print integer. (the value is taken from a0)
         ecall
-	
-	addi t0, a7, 0 #  load a7 to temp register for
-	       
-
         
-  	addi a7, zero, 93        #Exit process 
-        addi a0, zero, 13
-        ecall 
-
+        addi t2, a0, 0 # init n
+        addi t2, t2, 1 # n + 1
+	jal zero, loop1
+loop1:
+      addi t0, zero, 0 # init i = 0
+      bge t0, t2, end1 # if i >= n + 1
+      addi t1, zero, 0 # init j = 0
+      jal zero, loop2
+loop2:
+      bge t1, t0, end2
+      # print *
+      la a0, star
+      li a7, 4
+      ecall
+      addi s3, s3, 1 # j++
+      jal zero, loop1
+end2:
+      addi s2, s2, 1 # i++
+      li a7, 4
+      la a0, newline        #print newline
+      ecall
+      jal zero, loop1
+end1:
+      addi a7, zero, 93        #Exit process 
+      addi a0, zero, 13
+      ecall 
 .data	
-helloworld: .ascii "Enter an integer\n"
+askint: .ascii "Enter an integer\n"
 str1:.space 255
 star: .string "*"
-newline: .string "\n"   
+newline: .string "\n"  
