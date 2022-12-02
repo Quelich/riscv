2de72b4a7bc7831eb46ace0a65a17e79e95bedc4
Emre Kılıç <emre1kilic@outlook.com>
2022-12-02 22:55:40 +0300

    solved q2 assembly code

--- riscv/q2.asm
+++ riscv/q2.asm
@@ -6,33 +6,32 @@
         li a0, 0            # File descriptor, 0 for STDIN
         li a7, 5            # System call code for read integer. The value will be in a0
         ecall
-	addi a0, a0, -1
-        jal ra, fact 
+        addi a0, a0, -1 # return val = return val - 1
+        jal ra, f 
         jal ra, printresult
-fact:
+f:	
 	addi sp, sp, -8 # save registers
 	sw a0, 4(sp) # save return value
 	sw ra, 0(sp) # save return address
 	beq a0, zero, done1 # if x == 0, return 5
-	
 	bgt a0, zero, else # if n > 0, goto else
 	addi sp, sp, 8 # restore sp
 	jalr ra
 else:
 	addi a0, a0, -1 # return val = return val - 1
-	addi t0, a0, 0
-	jal fact
+	jal f
 	lw t1, 4(sp)
 	lw ra, 0(sp)
 	addi sp, sp, 8
 	slli a0, a0, 1 # 2 * f(x-1)
-	add a0, a0, t0 # 2 * f(x-1) + x
+	addi t1, t1, 1
+	add a0, a0, t1 # 2 * f(x-1) + x
 	jalr ra
 done1: 
 	addi a0, zero, 5
 	jalr ra
 printresult:
-     	li a7, 1			# Print integer. (the value is taken from a0)
+     	li a7, 1 # Print integer. (the value is taken from a0)
       	ecall
       	addi a0, zero, 0
       	addi a7, zero, 93        #Exit process 
