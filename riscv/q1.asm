750160235dbbf224f4bf110eafa0be13854cf9f5
Emre Kılıç <emre1kilic@outlook.com>
2022-12-02 14:47:01 +0300

    remove extra print integer

--- riscv/q1.asm
+++ riscv/q1.asm
@@ -7,9 +7,6 @@
         li a0, 0            # File descriptor, 0 for STDIN
         li a7, 5            # System call code for read integer. The value will be in a0
         ecall
-        
-        li a7, 1			# Print integer. (the value is taken from a0)
-        ecall
         addi t3, a0, 0 # init n 
         addi t0, zero, 1 # init i = 1
         jal ra, loop1 
